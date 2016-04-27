e5a41fad41d77c2127760f223706fd335fbf8911
Fajar Hidayat <fajarhide@gmail.com>
2016-04-27 11:07:33 +0700

    Init first

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# simple-page-maintenace-index
+# Simple-Page-Maintenace-Index
+
+Ini adalah halaman maintenance sederhana.
+
+## Bagaimana cara menginstall
+
+Cukup download index.php, memodifikasi isinya, dan salin sesuai keinginan Anda.
+
+Selain itu Anda dapat menggunakan script {install.sh} yang disediakan.
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+#status header customize
+header('Status: 200', TRUE, 200);
+
+#html echo php
+echo'<!DOCTYPE html>
+<html>
+    <head>
+        <title>Situs sedang development</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style type="text/css">
+            body { text-align: center; padding: 10%; font: 20px Helvetica, sans-serif; color: #333; }
+            h1 { font-size: 50px; margin: 0; }
+            article { display: block; text-align: left; max-width: 650px; margin: 0 auto; }
+            a { color: #dc8100; text-decoration: none; }
+            a:hover { color: #333; text-decoration: none; }
+            @media only screen and (max-width : 480px) {
+                h1 { font-size: 40px; }
+            }
+        </style>
+    </head>
+    <body>
+        <article>
+            <h2>Situs untuk sementara tidak tersedia.</h2>
+            <p>Masih dalam proses developement. Silahkan perikas kembali segera.</p>
+            <p>Kami mohon maaf atas ketidaknyamanan.</p>
+            <p id="signature">&mdash; <a href="mailto:[Email]">[Name]</a></p>
+        </article>
+    </body>
+</html>';
+
+?>
